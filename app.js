9ab972a8acd61bf3bd3d9b30ba06bfd6f61904cc
Shohag <tashohag1@gmail.com>
2023-03-22 00:14:44 +0300

    MARN Stack
    MARN

---
+++ app.js
@@ -0,0 +1,61 @@
+const express=require("express")
+const colors =require ('colors')
+const morgan=require("morgan")
+require ("dotenv").config()
+const cors=require('cors')
+const path=require('path')
+const app=express()
+const passport=require('passport')
+
+
+
+
+//Routers
+const userRoute=require("./routes/User.router")
+const DoctorRouter =require ('./routes/doctor.router')
+const adminRouter=require ('./routes/admin.router')
+
+
+//static file
+app.use(express.static(path.join(__dirname,'./client/build')))
+app.get('*',function(req,res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"))
+
+})
+
+
+
+
+
+// middelware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
+app.use(morgan("dev"))
+ app.use(cors())
+app.use(passport.initialize())
+
+require('./config/passport')
+
+//routes
+app.use('/user',userRoute)
+app.use('/doctor',DoctorRouter)
+app.use('/admin',adminRouter)
+//authntication
+app.get('/profile', passport.authenticate('jwt', { session: false }),
+    function(req, res) {
+return res.status(200).send({
+    success:true,
+    user:{
+        id:req.user._id,
+        username:req.user.name,
+        isAdmin:req.user.isAdmin,
+
+    }
+})
+
+    }
+);
+
+
+
+module.exports=app;
---
+++ server.js
@@ -0,0 +1,14 @@
+require ("dotenv").config()
+const connectDB = require("./config/db")
+
+
+//connet database
+
+connectDB()
+const app=require('./app')
+
+const PORT=process.env.PORT
+
+app.listen(PORT,()=>{
+    console.log(`server is running at http://localhost:${PORT}`.bgCyan.white);
+})
